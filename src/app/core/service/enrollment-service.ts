b39b5641f5a36df4cf98fbe42aa60765bc67710b
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-06 12:39:44 -0300

    feat: add getAllByEvent method in enrollment service

--- src/app/core/service/enrollment-service.ts
+++ src/app/core/service/enrollment-service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {ValidationResponse} from '../types/ValidationResponse';
 import {environment} from '../../../environment/enviroment';
+import {Pagination} from '../types/Pagination';
+import {Enrollment} from '../types/Enrollment';
 
 @Injectable({
   providedIn: 'root'
@@ -23,5 +25,16 @@ export class EnrollmentService {
     };
 
     return this.http.post<ValidationResponse>(`${this.apiUrl}/validate/code`, requestBody);
+  }
+
+  getAllByEvent(eventId: number, query: string, page = 0, size = 0): Observable<Pagination<Enrollment>> {
+    const params = [
+      `query=${query}`,
+      `page=${page}`,
+      `size=${size}`
+    ].filter(param => param !== '')
+    .join('&');
+
+    return this.http.get<Pagination<Enrollment>>(`${this.apiUrl}/get-all-by-event/${eventId}?${params}`);
   }
 }
