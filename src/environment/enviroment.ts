a8e7e409313f8073208dd4980eaf4fe21639d6da
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-08 11:57:25 -0300

    API URL

--- src/environment/enviroment.ts
+++ src/environment/enviroment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080'
-  // apiUrl: 'https://events-backend-c8sl.onrender.com'
+  // apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://events-backend-c8sl.onrender.com'
 };
