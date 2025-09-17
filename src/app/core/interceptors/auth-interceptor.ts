a678fb5f5e1a131126c1e7d3f81b9fb80adabf4a
Wesley00s <wesley300rodrigues@gmail.com>
2025-09-17 11:07:59 -0300

    ops: add cache control in interceptor headers

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -3,6 +3,7 @@ import {inject, PLATFORM_ID} from '@angular/core';
 import {isPlatformBrowser, isPlatformServer} from '@angular/common';
 import {CookieService} from 'ngx-cookie-service';
 import {requestContext} from '../../request-context.service';
+import {environment} from '../../../environment/enviroment';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const platformId = inject(PLATFORM_ID);
@@ -11,22 +12,31 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   if (isPlatformBrowser(platformId)) {
     const cookieService = inject(CookieService);
     token = cookieService.get('token');
+  }
 
-    if (isPlatformServer(platformId)) {
-      const request = requestContext.get();
+  if (isPlatformServer(platformId)) {
+    const request = requestContext.get();
 
-      if (request && request.cookies) {
-        token = request.cookies['token'];
-      }
+    if (request && request.cookies) {
+      token = request.cookies['token'];
     }
   }
 
+  let headers = req.headers;
+
   if (token) {
-    const clonedReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
-    });
-    return next(clonedReq);
+    headers = headers.set('Authorization', `Bearer ${token}`);
   }
 
-  return next(req);
+  const noCacheUrl = `${environment.apiUrl}/event/get-event-active`;
+  if (req.method === 'GET' && req.url === noCacheUrl) {
+    headers = headers
+      .set('Cache-Control', 'no-cache, no-store, must-revalidate')
+      .set('Pragma', 'no-cache')
+      .set('Expires', '0');
+  }
+
+  const clonedReq = req.clone({ headers });
+
+  return next(clonedReq);
 };
